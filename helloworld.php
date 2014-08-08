16285c4a24679a33700bf39e16efebca42b1cba2
Sam Dowd <sam@samueldowd.com>
2014-08-08 14:10:24 -0400

    One last time?

--- helloworld.php
+++ helloworld.php
@@ -12,7 +12,7 @@
     ?>
     <form action="/sign" method="post">
       <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="Sign Guestbook"></div>
+      <div><input type="submit" value="Sign Guestbook..."></div>
     </form>
   </body>
 </html>
